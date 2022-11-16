79e277c619ad3755e71c948d9f50eae6ef8ff442
zhangxin <1129170176@qq.com>
2022-11-17 00:09:46 +0800

    增加部分token代码

--- utility/token/token.go
+++ utility/token/token.go
@@ -3,8 +3,11 @@ package token
 import (
 	"context"
 	"github.com/gogf/gf/v2/container/gmap"
+	"github.com/gogf/gf/v2/crypto/gaes"
+	"github.com/gogf/gf/v2/encoding/gbase64"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
+	"github.com/gogf/gf/v2/util/guid"
 	"go_ticket/internal/errorcode"
 	"strings"
 )
@@ -18,6 +21,20 @@ var (
 type MyToken struct {
 	Timeout   int // 超时时间(毫秒)
 	CacheMode int // 缓存类型: 内存1,redis2
+}
+type MyCacheToken struct {
+	Token         string
+	UserKey       string
+	Uuid          string
+	UserData      interface{}
+	CreatedAt     int64 // Token 生成的时间
+	NextFreshTime int64 // 下次token刷新时间, =0，一次性token
+}
+
+type MyRequestToken struct {
+	UserKey string
+	Uuid    string
+	Token   string
 }
 
 // Instance 获取配置文件Token的配置信息
@@ -60,4 +77,44 @@ func GetRequestToken(r *ghttp.Request) (token string, err error) {
 
 	}
 	return "", nil
+}
+
+/**
+* ValidToken
+*  @Description: 验证token
+*  @param ctx
+*  @param token
+*  @return MyCacheToken
+ */
+//func (m *MyToken) ValidToken(ctx context.Context, token string) *MyCacheToken {
+//
+//}
+
+/**
+* EncryptToken
+*  @Description:加密生成token.
+               token的生成规则是base64(gaes.Encrypt(base64(userKey)+TokenDelimiter+uuid)): 其中TokenDelimiter默认为_;
+               为什么要base64(userKey)，因为可能userKey包含_; 标准base64是使用 `数字`+`大小写字母`+`/`+`+`以及`=`组成
+               解释：为什么还要对token进行加解密？答：加密因为token携带了userKey信息，且便于过滤掉不合法token；
+*  @param ctx
+*  @param userKey 用户唯一标识
+*  @return *MyRequestToken
+*  @return error
+*/
+func (m *MyToken) EncryptToken(ctx context.Context, userKey string) (*MyRequestToken, error) {
+	if userKey == "" {
+		return nil, errorcode.NewMyErr(ctx, errorcode.TokenEmpty)
+	}
+	uuid := guid.S()
+	tokenstr := gbase64.EncodeToString([]byte(userKey)) + TokenDelimiter + uuid
+
+	token, err := gaes.Encrypt([]byte(tokenstr), []byte(EncryptKey))
+	if err != nil {
+		return nil, errorcode.NewMyErr(ctx, errorcode.MyInternalError, err)
+	}
+	return &MyRequestToken{
+		UserKey: userKey,
+		Uuid:    uuid,
+		Token:   gbase64.EncodeToString(token),
+	}, nil
 }
